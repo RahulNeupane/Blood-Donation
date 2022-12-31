f185a4be28f4b138675cc7705d478a7d10adf870
RahulNeupane <nrahul503@gmail.com>
2022-12-31 21:05:14 +0545

    carousel finished

--- resources/views/indexed.blade.php
+++ resources/views/indexed.blade.php
@@ -1,16 +1,13 @@
 @extends('layouts.app')
 @section('content')
     <div class="hero-section">
-        <img src="{{url(asset('images/slider.jpg'))}}" class="d-block w-100 z-1" alt="..." style="height: 90vh">
+        <img src="{{url(asset('images/slider.jpg'))}}" class="d-block w-100 z-1"  style="height: 90vh">
         <div class="details">
             <h1>Blood is a life,<br>pass it on !</h1>
             <p>Help us eliminate blood scarcity in Nepal</p>
             <a href=""><span>Donate Now <i class='bx bxs-right-arrow'></i></span></a>
         </div>
-        <div class="wave">
-            <img src="{{url(asset('images/wave.svg'))}}" alt="">    
-        </div>
-
+        <img src="{{url(asset('images/wave.svg'))}}" class="wave">    
     </div>
       
 @endsection
