8ff171685eaf62f9c42c04bb19ebddd4b0ecabe8
RahulNeupane <nrahul503@gmail.com>
2022-12-31 20:41:54 +0545

    carousel

--- resources/views/indexed.blade.php
+++ resources/views/indexed.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app')
 @section('content')
-    <div class="hero-section position-relative">
-        <img src="{{url(asset('images/20221226075544.jpg'))}}" class="d-block w-100 z-1" alt="..." style="height: 90vh">
-        <div class="details position-absolute">
-            <h1>Welcome to Our Website</h1>
-            <p>We are a team of experienced professionals dedicated to providing top-quality services to our clients.</p>
-            <button class="btn btn-primary">Learn More</button>
+    <div class="hero-section">
+        <img src="{{url(asset('images/slider.jpg'))}}" class="d-block w-100 z-1" alt="..." style="height: 90vh">
+        <div class="details">
+            <h1>Blood is a life,<br>pass it on !</h1>
+            <p>Help us eliminate blood scarcity in Nepal</p>
+            <a href=""><span>Donate Now <i class='bx bxs-right-arrow'></i></span></a>
         </div>
+        <div class="wave">
+            <img src="{{url(asset('images/wave.svg'))}}" alt="">    
+        </div>
+
     </div>
       
 @endsection
