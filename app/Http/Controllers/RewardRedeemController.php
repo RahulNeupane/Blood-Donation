8c576d32bc759b74c7930f6d41c67fa6ad7e5fb9
RahulNeupane <rahulneupane0090@gmail.com>
2023-02-19 16:55:20 +0545

    redeem confirm show

--- app/Http/Controllers/RewardRedeemController.php
+++ app/Http/Controllers/RewardRedeemController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reward;
 use Illuminate\Http\Request;
 
 class RewardRedeemController extends Controller
 {
-    //
+    public function index($id){
+        $reward= Reward::findOrFail($id);
+        return view('redeem',compact('reward'));
+
+    }
 }
