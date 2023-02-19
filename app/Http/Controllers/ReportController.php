9280a27850ceb3c1282edb9c072b474d1ed135ed
RahulNeupane <rahulneupane0090@gmail.com>
2023-02-19 18:59:52 +0545

    donate report generated

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Requests;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
     public function donateReports(){
-        return view('reports.donate');
+        $donate = Requests::where('type', '=', 1)->where('approve', '=', 1)->get();
+        return view('reports.donate',compact('donate'));
     }
     public function receiveReports(){
         return view('reports.receive');
